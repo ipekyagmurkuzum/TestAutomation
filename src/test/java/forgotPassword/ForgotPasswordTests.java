6d4a7b50b96754d959a637bd65bd682ef4d81833
yagmur <yagmurkuzuum@gmail.com>
2022-06-02 23:06:37 +0300

    practice

--- src/test/java/forgotPassword/ForgotPasswordTests.java
+++ src/test/java/forgotPassword/ForgotPasswordTests.java
@@ -4,14 +4,9 @@ import base.BaseTests;
 import org.testng.annotations.Test;
 import pages.ForgotPasswordPage;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
-
 public class ForgotPasswordTests extends BaseTests {
-
     @Test
-    public void testForgotPassword() throws InterruptedException {
-
+    public void testForgotPassword(){
         ForgotPasswordPage forgotPasswordPage = homePage.clickForgotPassword();
         forgotPasswordPage.setEmail("tau@example.com");
         forgotPasswordPage.clickOnRetrievePassword();
