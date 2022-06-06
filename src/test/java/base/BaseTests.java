30deb1feca1510c666af300b183cab80b8bb39c7
yagmur <yagmurkuzuum@gmail.com>
2022-06-06 17:35:28 +0300

    practice

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -7,6 +7,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 
+import java.time.Duration;
+
 public class BaseTests {
     private WebDriver driver;
     protected HomePage homePage;
@@ -15,13 +17,15 @@ public class BaseTests {
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
+//      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
         goHome();
-        homePage = new HomePage(driver);
     }
 
     @BeforeMethod
     public void goHome() {
         driver.get("https://the-internet.herokuapp.com/");
+        homePage = new HomePage(driver);
     }
 
     @AfterClass
